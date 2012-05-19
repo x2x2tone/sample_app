6c6c8ba867efaf9a9bce39ea55a25424139c133e
@2C <x2x2tone@gmail.com>
2012-05-19 16:49:28 +0900

    Add gem Guard & Spork
    Add a title helper

--- Gemfile
+++ Gemfile
@@ -1,14 +1,15 @@
 source 'https://rubygems.org'
 
 gem 'rails', '3.2.3'
+gem 'pg', '0.12.2'
 
 # Bundle edge Rails instead:
 # gem 'rails', :git => 'git://github.com/rails/rails.git'
 
 group :development, :test do
 	#gem 'sqlite3', '1.3.5'
-	gem 'pg', '0.12.2'
 	gem 'rspec-rails', '2.9.0'
+	gem 'guard-rspec', '0.5.5'
 end
 
 
@@ -27,12 +28,17 @@ end
 gem 'jquery-rails', '2.0.0'
 
 group :test do
+	gem 'rspec-rails', '2.9.0'
 	gem 'capybara', '1.1.2'
+	gem 'rb-inotify', '0.8.8'
+	gem 'libnotify', '0.5.9'
+	gem 'guard-spork', '0.3.2'
+	gem 'spork', '0.9.0'
 end
 
-group :production do
-	gem 'pg', '0.12.2'
-end
+#group :production do
+#	gem 'pg', '0.12.2'
+#end
 
 # To use ActiveModel has_secure_password
 # gem 'bcrypt-ruby', '~> 3.0.0'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,17 +2,21 @@ require 'spec_helper'
 
 describe "StaticPages" do
  
- 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-	
 	describe "Home page" do
     it "should have the content 'Sample App'" do
       visit '/static_pages/home'
 			page.should have_selector('h1', :text => 'Sample App')
     end
-		
-		it "should have the right title" do
+
+		it "should have the base title" do
 			visit '/static_pages/home'
-			page.should have_selector('title', :text => "#{base_title} | Home")
+			page.should have_selector('title', 
+			                     :text => "Ruby on Rails Tutorial Sample App")
+		end
+		
+		it "should not have a custom page title" do
+			visit '/static_pages/home'
+			page.should_not have_selector('title', :text => "| Home")
 		end
   end
 
