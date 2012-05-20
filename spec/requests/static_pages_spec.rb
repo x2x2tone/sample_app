b910bbc617db4268d27080d0dd5ae4e876c16e15
@2C <x2x2tone@gmail.com>
2012-05-20 16:19:22 +0900

    Finish StaticPages routes

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,22 +6,15 @@ describe "StaticPages" do
 #	end
 
 	let(:base_title) { "Ruby on Rails Tutorial Sample App" }
-	describe "Home page" do
-    it "should have the content 'Sample App'" do
-      visit root_path
-			page.should have_selector('h1', :text => 'Sample App')
-    end
 
-		it "should have the base title" do
-			visit root_path
-			page.should have_selector('title', 
-			                     :text => "Ruby on Rails Tutorial Sample App")
-		end
-		
-		it "should not have a custom page title" do
-			visit root_path
-			page.should_not have_selector('title', :text => "| Home")
-		end
+	subject { page }
+	describe "Home page" do
+    before { visit root_path }
+
+		it { should have_selector('h1', text: 'Sample App') }
+		it { should have_selector('title', 
+		                           text: "Ruby on Rails Tutorial Sample App") }
+		it { should_not have_selector('title', :text => "| Home") }
   end
 
   describe "Help page" do
