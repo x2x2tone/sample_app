3b767b7f8fd527a8f3a372fd64ab616a6055dadf
@2C <x2x2tone@gmail.com>
2012-05-31 16:10:49 +0900

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 SampleApp::Application.routes.draw do
-  get "users/new"
 
+	resources :users
   root to: "static_pages#home"
 
   match '/signup', to: "users#new"
@@ -8,6 +8,7 @@ SampleApp::Application.routes.draw do
   match '/help', to: "static_pages#help"
   match '/about', to: "static_pages#about"
   match '/contact', to: "static_pages#contact"
+
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
