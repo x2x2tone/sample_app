de5cc8cb5470bf59de6c15a90b5735c50a0f8512
@2C <x2x2tone@gmail.com>
2012-06-14 17:50:20 +0900

    Finish user edit, update, index, and destroy actions

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,7 +14,7 @@ require 'spec_helper'
 describe User do
 	before do
 	  @user = User.new(name: "Example User", email: "user@example.com",
-		                 password: "foobar", password_confirmation: "foobar") 
+		                 password: "foobar", password_confirmation: "foobar")
 	end
 	
 	subject { @user }
@@ -25,8 +25,10 @@ describe User do
 	it { should respond_to(:password_confirmation) }
 	it { should respond_to(:remember_token) }
 	it { should respond_to(:authenticate) }
+	it { should respond_to(:admin) }
 
 	it { should be_valid }
+	it { should_not be_admin }
 
 	describe "when name is not present" do
 		before { @user.name = " " }
@@ -113,6 +115,11 @@ describe User do
 	describe "remember token" do
 		before { @user.save }
 		its(:remember_token) { should_not be_blank }
+	end
+
+	describe "admin user" do
+		before { @user.toggle!(:admin) }
+		it { should be_admin }
 	end
 
 end
